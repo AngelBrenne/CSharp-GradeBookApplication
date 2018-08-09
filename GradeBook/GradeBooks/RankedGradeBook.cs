4e1dc13c625ee979fe80898dc00e846f62f63d38
AngelBrenne <angel_daling@hotmail.com>
2018-08-09 13:37:23 +0200

    Task 3.4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,9 +5,9 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook: BaseGradeBook 
+    public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base (name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
@@ -17,7 +17,49 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
-            return 'F';
+
+            int downOneGrade = Students.Count / 5;
+            int placement = -1;
+            List<double> gradesList = new List<double>();
+
+            foreach (Student student in Students)
+            {
+                gradesList.Add(student.AverageGrade);
+            }
+            gradesList.Sort();
+            for (int i = 0; i < gradesList.Count; i++)
+            {
+                if (averageGrade == gradesList[i])
+                {
+                    placement = i;
+                }
+            }
+
+            if (placement == -1)
+            {
+                Console.WriteLine("Something went wrong, ops");
+                return 'Q';
+            }
+            else if (placement < downOneGrade)
+            {
+                return 'F';
+            }
+            else if (placement < downOneGrade * 2)
+            {
+                return 'D';
+            }
+            else if (placement < downOneGrade * 3)
+            {
+                return 'C';
+            }
+            else if (placement < downOneGrade * 4)
+            {
+                return 'B';
+            }
+            else
+            {
+                return 'A';
+            }
         }
     }
 }
